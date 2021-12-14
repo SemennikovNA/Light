b2debaf751e64960cba9a15aae75376dff38c4bf
Nikita Semennikov <semennikovna@ya.ru>
2021-12-14 21:50:13 +0300

    Initial Commit

---
+++ TheLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheLight
+//
+//  Created by Никита on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
